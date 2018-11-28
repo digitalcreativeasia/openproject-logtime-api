d349ccd8978ab38df18bd788f757303209a10ca5
hynra <hynra@yahoo.com>
2018-11-28 13:15:22 +0700

    init koa

---
+++ app.js
@@ -0,0 +1,29 @@
+const logger = require('koa-logger');
+const router = require('koa-router')();
+const onerror = require('koa-onerror')
+
+const Koa = require('koa');
+const app = module.exports = new Koa();
+
+onerror(app)
+
+app.use(logger());
+
+router.post('/', add);
+
+app.use(router.routes());
+
+
+async function add(ctx) {
+  ctx.body = {"test": "test"}
+}
+
+
+
+
+app.on('error', (err, ctx) => {
+    ctx.body = {"error": "internal server error"}
+  });
+
+
+if (!module.parent) app.listen(3000);
