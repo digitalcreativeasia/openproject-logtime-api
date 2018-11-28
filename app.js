3f80efbbf96050d52cfb499ad425508d0f0ba286
hynra <hynra@yahoo.com>
2018-11-28 15:39:24 +0700

    define auth

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const onerror = require('koa-onerror')
+const auth = require('basic-auth')
+const axios = require('axios');
+const bodyparser = require('koa-bodyparser')
+const json = require('koa-json')
 
-const { dbcredentials } = require('./config')
+const { dbcredentials, api_url } = require('./config')
 
 const Koa = require('koa');
 const app = module.exports = new Koa();
 
 onerror(app)
+
+app.use(bodyparser({
+    enableTypes: ['json', 'form', 'text']
+}))
+app.use(json())
 
 app.use(logger());
 
@@ -17,12 +26,51 @@ app.use(router.routes());
 
 
 async function add(ctx) {
-    ctx.body = { "test": "test" }
+    //check 
+    var user = auth(ctx.request)
+    let response = await axios({
+        method: 'get',
+        url: api_url,
+        auth: {
+            username: user.name,
+            password: user.pass
+        }
+    })
+    if (response.status === 200) {
+        let body = ctx.request.body
+        let project_id = parseInt(body['project_id'])
+        let user_id = parseInt(body['user_id'])
+        let work_package_id = parseInt(body['work_package_id'])
+        let hours = parseFloat(body['hours'])
+        let comments = body['comments']
+        let activity_id = parseInt(body['activity_id'])
+        let spent_on = STR_TO_DATE(body['spent_on'], '%Y-%m-%d')
+        let tyear
+        let tmonth
+        let tweek
+        let created_on = new Date()
+        let updated_on = new Date()
+        
+        //let overridden_costs
+        //let costs
+        //let rate_id
+    } else {
+        ctx.status = 401
+        ctx.body = {
+            "_type": "Error",
+            "errorIdentifier": "urn:openproject-org:api:v3:errors:Unauthenticated",
+            "message": "Unauthenticated"
+        }
+    }
 }
+
+
 
 app.on('error', (err, ctx) => {
     ctx.body = { "error": "internal server error" }
 });
+
+
 
 
 if (!module.parent) app.listen(4000);
