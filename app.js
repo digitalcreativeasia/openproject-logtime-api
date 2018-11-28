7a73e40d7bd3bb257c78df84d4b16919ba4990c6
hynra <hynra@yahoo.com>
2018-11-28 19:08:29 +0700

    enumtypes api

--- app.js
+++ app.js
@@ -22,9 +22,42 @@ app.use(json())
 
 app.use(logger());
 
-router.post('/', add);
+router.post('/', add)
+    .get('/enum', enumTypes);
 
 app.use(router.routes());
+
+
+
+async function enumTypes(ctx) {
+    //check 
+    var user = auth(ctx.request)
+    let response = await axios({
+        method: 'get',
+        url: api_url,
+        auth: {
+            username: user.name,
+            password: user.pass
+        }
+    })
+    if (response.status === 200) {
+
+        let conn = await mysql.createConnection(dbcredentials)
+        let result = await conn.query('SELECT * FROM enumerations WHERE type="TimeEntryActivity"')
+        conn.end();
+        console.log(result)
+        ctx.body = result
+    } else {
+        ctx.status = 401
+        ctx.body = {
+            "_type": "Error",
+            "errorIdentifier": "urn:openproject-org:api:v3:errors:Unauthenticated",
+            "message": "Unauthenticated"
+        }
+    }
+}
+
+
 
 
 async function add(ctx) {
@@ -51,20 +84,14 @@ async function add(ctx) {
         let tweek = parseInt(moment().format('W'))
         let created_on = new Date()
         let updated_on = new Date()
-        
+
         let conn = await mysql.createConnection(dbcredentials)
-        let result = conn.query('INSERT INTO time_entries (project_id, user_id, work_package_id, hours, comments, activity_id, spent_on, tyear, tmonth, tweek, created_on, updated_on) '+
-        'VALUES ("'+project_id+'", "'+user_id+'", "'+work_package_id+'", "'+hours+'", "'+comments+'", "'+activity_id+'", NOW(), "'+tyear+'", "'+tmonth+'", "'+tweek+'", NOW(), NOW())')
+        let result = await conn.query('INSERT INTO time_entries (project_id, user_id, work_package_id, hours, comments, activity_id, spent_on, tyear, tmonth, tweek, created_on, updated_on) ' +
+            'VALUES ("' + project_id + '", "' + user_id + '", "' + work_package_id + '", "' + hours + '", "' + comments + '", "' + activity_id + '", NOW(), "' + tyear + '", "' + tmonth + '", "' + tweek + '", NOW(), NOW())')
         conn.end();
         console.log(result)
-        //let overridden_costs
-        //let costs
-        //let rate_id
-
-
-
         ctx.body = {
-            "test": "test"
+            "status": "success"
         }
     } else {
         ctx.status = 401
